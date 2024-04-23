5901ce0bc50d26459f0bcb4de9037b31b80c6799
grlainne <148122985+grlainne@users.noreply.github.com>
2024-04-23 03:30:18 -0600

    Second Commit

---
+++ java.js
@@ -0,0 +1,17 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+let currentYear = new Date().getFullYear();
+              document.getElementById("year").innerText = `© ${currentYear} Gail Salinas`;
